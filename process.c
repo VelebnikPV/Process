c3dab4a86a3f31bba7bcf8045ef0f371b09a6067
Polina Velebnik <velebnik@gmail.com>
2017-06-15 23:35:35 +0300

    Deleted comment

--- process.c
+++ process.c
@@ -3,18 +3,6 @@
 #include <unistd.h>
 int main() 
 {
-/*pid_t child_pid;
-printf("The main program process ID is %d\n",(int)getpid());
-child_pid = fork();
-	if (child_pid != 0) 
-	{
-	printf("This is the parent process, with ID %d\n", (int)getpid());
-	printf("The child's process ID is %d\n", (int)child_pid);
-	} 
-	else 
-	{
-	printf("This is the child process, with ID %d\n", (int)getpid());
-	}*/
 char *argv[3] = {"Command-line", ".", NULL};
 
 	int pid = fork();
